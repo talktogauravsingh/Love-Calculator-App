002e21172cd544d26313f25fbd74e2ca2d049bc1
Gaurav <talktoogaurav@gmail.com>
2020-09-18 13:07:18 -0500

    file upldated successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'lovecalcApp';
+  title = 'Love Calculator App';
+  public lvpercent :string;
+
+  onClickSubmit(loverDetails : { yname:string, pname:string }){
+
+      var yname = loverDetails.yname.replace(/\s/g, "").length;
+      var pname = loverDetails.pname.replace(/\s/g, "").length;
+
+      if(yname >=10){
+        alert("Yname length exceeds from 9");
+        yname = Math.round(yname/2);
+      }
+      
+      if(pname >=10){
+        alert("Pname length exceeds from 9");
+        pname = Math.round(pname/2);
+      }
+
+      this.lvpercent = `${yname}` + pname + "%";
+
+  }
+
 }
